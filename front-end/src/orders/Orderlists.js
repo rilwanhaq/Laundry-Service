5bb3d14993fcf3e91c69ed45359786bcb32865e7
Dnk12652 <devadurgam52@gmail.com>
2022-03-25 20:59:54 +0530

    orderlists_commits

--- front-end/src/orders/Orderlists.js
+++ front-end/src/orders/Orderlists.js
@@ -1,55 +1,72 @@
-import React, { useEffect, useState } from 'react'
+import React,{useState} from 'react'
 
 import {useHistory,Link} from "react-router-dom"
 import "../orders/Orderlists.css"
 import Product_type_lists from './Product_type_lists'
+import Summary from '../Summary/SummaryToCreate'
+import SummaryToCreate from '../Summary/SummaryToCreate'
 
 
 
 
 
-
+let totalorders={ orderId: "orderId", userId: "userId",details: new Map()  }
+let OrderedItems=[]
 function Orderlists() {
-  const [totalorders,setOrders]=useState([]) 
-  const callbackFunction=()=>{
+  const [showSummary,setSummary]=useState(false)
 
+ function  callbackFunction(props){
+  totalorders.details.set(props.productType,props.value)
+ OrderedItems = [...totalorders.details].map(([ productType,value])=>({
+    productType,
+      value,
+    }))
+
+return 
   }
  
   const orderdata = [
         {
         productname :'shirts',
         description:"selcet noof shirts to placeorder to laundry ",
-        image:'https://th.bing.com/th/id/OIP.kZUnFP1eViE_45Y_czSNbgHaLH?pid=ImgDet&rs=1'
+        image:'https://th.bing.com/th/id/OIP.kZUnFP1eViE_45Y_czSNbgHaLH?pid=ImgDet&rs=1',
+        id:1
         },
       {
       productname :'Tshirts',
       description:"selcet noof Tshirts to placeorder to laundry ",
-      image:'https://th.bing.com/th/id/OIP.gtRV3hBK_e7BhJsLoTkTuAHaI4?pid=ImgDet&rs=1'
+      image:'https://th.bing.com/th/id/OIP.gtRV3hBK_e7BhJsLoTkTuAHaI4?pid=ImgDet&rs=1',
+      id:2
         },
         {
         productname :'Trousers',
         description:"selcet Trousers shirts to placeorder to laundry ",
-        image:'https://th.bing.com/th/id/OIP.yCMVmqhpIPDcGRdQA7uiswHaJ4?pid=ImgDet&rs=1'
+        image:'https://th.bing.com/th/id/OIP.yCMVmqhpIPDcGRdQA7uiswHaJ4?pid=ImgDet&rs=1',
+        id:3
           },
       {
         productname :'Jeans',
         description:"selcet noof Jeans to placeorder to laundry ",
-        image:'https://th.bing.com/th/id/OIP.bkNXpIdRAszLa8_BWYWQlgHaMH?pid=ImgDet&rs=1'
+        image:'https://th.bing.com/th/id/OIP.bkNXpIdRAszLa8_BWYWQlgHaMH?pid=ImgDet&rs=1',
+        id:4
           },
           {
             productname :'Boxers',
             description:"selcet noof Boxers to placeorder to laundry ",
-            image:'https://th.bing.com/th/id/OIP.0rS-XGejGDz2fga7UeptsgHaH3?pid=ImgDet&rs=1'
+            image:'https://th.bing.com/th/id/OIP.0rS-XGejGDz2fga7UeptsgHaH3?pid=ImgDet&rs=1',
+            id:5
           },
           {
           productname :'Joggers',
           description:"selcet noof Joggers to placeorder to laundry ",
-          image:'https://th.bing.com/th/id/OIP.-1XpE5CXYnblCkXoWUrvIwHaIY?pid=ImgDet&rs=1'
+          image:'https://th.bing.com/th/id/OIP.-1XpE5CXYnblCkXoWUrvIwHaIY?pid=ImgDet&rs=1',
+          id:6
           },
           {
           productname :'others',
           description:"selcet noof others to placeorder to laundry ",
-          image:'https://th.bing.com/th/id/OIP.nPcc7GDQZfugMeQr9RoSnQHaHU?pid=ImgDet&w=800&h=790&rs=1'
+          image:'https://th.bing.com/th/id/OIP.nPcc7GDQZfugMeQr9RoSnQHaHU?pid=ImgDet&w=800&h=790&rs=1',
+          id:7
           },
     ]
   return (
@@ -96,9 +113,16 @@ function Orderlists() {
                     </thead>
                     <tbody>
                   { orderdata.map((data)=>{
-                  return (
-                    <Product_type_lists gettingOrders={ callbackFunction} data={data}/>
-                  )})}
+                
+                 return( <Product_type_lists
+                  
+                     
+                       image={data.image}
+                       description={data.description}
+                       productType={data.productname}
+                       gettingOrders={ callbackFunction}
+                       />
+                   ) })}
                     
                     </tbody> 
                   </table>
@@ -109,8 +133,10 @@ function Orderlists() {
           </div>
           <div  className='proceed-cancel-btn'>
               <button className='cancel-btn'>Cancel</button>
-              <button className='proceed-btn'>Proceed</button>
+              <button onClick={()=>{setSummary(true)}} className='proceed-btn'>Proceed</button>
             </div>
+
+          {showSummary && <SummaryToCreate showSummary={showSummary}  OrderedItems={ OrderedItems}/>}
       <footer className='footer-component'>
                     <p>2022</p>
                     <div className='footer-circle'>C</div>
