c443422dc325537619ca02d9cc69614907276750
Rilwan_Haq <rilwanhaq@gmail.com>
2022-03-28 16:42:17 +0530

    final

--- Back-end/app.js
+++ Back-end/app.js
@@ -29,7 +29,7 @@ mongoose.connect(MONGO_URI)
     console.log(err.message);
 });
 
-app.use(cors())
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
